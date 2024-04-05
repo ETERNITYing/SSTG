e90872f1306d7dae351e568ceb8e8bac6a172f43
ETERNITYing <93258551+ETERNITYing@users.noreply.github.com>
2024-04-05 17:09:19 +0800

    Delete src/main/java directory

--- src/main/java/display/fontConfig.java
+++
@@ -1,8 +0,0 @@
-package main.java.display;
-
-import java.awt.*;
-
-public class fontConfig {
-    final Font titalFont = new Font("BIZ UDMincho",Font.BOLD,30);
-    final Font versionFont = new Font("Cambira Math",Font.ITALIC,12);
-}
